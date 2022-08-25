9ba7c608157151056b49459c7cbd051fb73cfc30
Megan <meganmoulos@gmail.com>
2022-08-25 11:02:47 -0700

    commit tree 4a3a56dcf81dc945069b7808a354a75ea1d4743a

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9- ']+/g, "")
+  }
+
+  static titleize(string){
+    let arr = []
+    let excludeWords = ['a', 'an', 'the', 'of', 'but', 'for', 'at', 'by', 'and', 'from']
+    arr = string.split(' ')
+    return arr.map((word, i) => {
+      return excludeWords.includes(word) && i > 0 ? [word] : word.charAt(0).toUpperCase() + word.slice(1)
+    }) .join(' ')
+  }
 }
